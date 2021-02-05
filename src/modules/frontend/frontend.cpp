c7b18767b22b23a1cbc2d25cb2551d4ba5589293
Jakub Piskiewicz <jakub@piskiewicz.org>
2021-02-05 13:42:26 +0100

    Set up the connection

--- src/modules/frontend/frontend.cpp
+++ src/modules/frontend/frontend.cpp
@@ -1,11 +1,39 @@
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <netdb.h>
 #include <cstring>
 #include <iostream>
+#include <cstdlib>
 
 #define BACKLOG 10
 #define PORT "8080"
+
+/* Wait for terminated childern processes to release the system resources
+ * associated with them. See man wait(2) for details. */
+void sigchld_handler(int s) {
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    // "waitpid()" returns 0 if there are no children left to wait for.
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+/* Get socket's address either in IPv4 or IPv6.
+ * We are returning a void pointer because
+ * we don't know if this is "sockaddr_in6" or
+ * "sockaddr_in" in advance. */
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int main() {
     /* This module will listen for incoming connections
@@ -19,8 +47,14 @@ int main() {
      * to be accepted. */
 
     // Error code and socket file descriptor
-    int status, sockfd;
+    int status; // Status of getaddrinfo
+    int sockfd, new_fd; // socket file descriptor and accepted socket's FD
     struct addrinfo hints, *res; // *res is the result of getaddrinfo()
+    struct addrinfo *p; // *p is used to loop through linked list of addresses in *res
+    struct sockaddr_storage client_addr; // "sockaddr_storage" is big enough for both address types.
+    socklen_t sin_size; // Size of the address.
+    int yes = 1; // This is used to set option value in "setsockopt()".
+    struct sigaction sa; // Eliminate zombie processes. See man sigaction(2) and man wait(2).
 
     // Reset the value of hints
     std::memset(&hints, 0, sizeof hints);
@@ -34,27 +68,63 @@ int main() {
         return 1;
     }
 
-    // Get the socket file descriptor
-    if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
-       int err = errno;
-       std::cerr << "error while getting the socket file descriptor: " << err << "\n";
-       return 1;
+    // Get the socket file descriptor. Try first address that works.
+    for (p = res; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            int err = errno;
+            std::cerr << "error while getting the socket file descriptor: " << err << "trying on next address.\n";
+            continue;
+        }
+
+        /* Set options for that socket.
+         * "SOL_SOCKET" means that "setsockopt" sets options at socket level. */
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) != 0) {
+            int err = errno;
+            std::cerr << "setsockopt error: " << err << "\n";
+            exit(1); // Close all file descriptors and terminate program.
+        }
+
+        // Bind socket to port specified in "getaddrinfo"
+        if (bind(sockfd, res->ai_addr, res->ai_addrlen) != 0) {
+            close(sockfd);
+            int err = errno;
+            std::cerr << "bind error: " << err << "\n";
+            continue;
+        }
+
+        // Stop looking if the address has been found.
+        break;
     }
 
-    // Bind socket to port specified in "getaddrinfo"
-    if (bind(sockfd, res->ai_addr, res->ai_addrlen) != 0) {
-        int err = errno;
-        std::cerr << "bind error: " << err << "\n";
-        return 1;
+    freeaddrinfo(res); // Free the linked list
+
+    if (p == NULL) {
+        std::cerr << "failed to bind socket to port.\n";
+        exit(1);
     }
 
     // Listen for incoming connecting
     if (listen(sockfd, BACKLOG) != 0) {
         int err = errno;
         std::cerr << "listen error: " << err << "\n";
-        return 1;
+        exit(1);
     }
 
-    freeaddrinfo(res); // Free the linked list
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART; // Make system calls restartable. See man sigaction(2) and man signal(7).
+    if (sigaction(SIGCHLD, &sa, NULL) != 0) {
+        int err = errno;
+        std::cerr << "sigaction error" << errno << "\n";
+        exit(1);
+    }
+
+    std::cout << "Server ready. Listening on port: " << PORT << ".\n";
+
+    // Now the fun begins!
+    while(1) {
+        sin_size = sizeof client_addr;
+    }
+
     return 0;
 }
