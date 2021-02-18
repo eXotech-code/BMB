89cfca8c35b8e396f02bdbbd1c40e1722a26dbfd
Jakub Piskiewicz <jakub@piskiewicz.org>
2021-02-18 18:09:02 +0100

    Started work on resolver functions

--- src/modules/frontend/queries.h
+++ src/modules/frontend/queries.h
@@ -13,4 +13,25 @@
 // Checks if this is a valid query and extracts it from the HTTP request.
 std::string extract_query(std::string request);
 
+// Query resolver functions and structs.
+
+// Post struct as per my notes.
+// TODO: Make a handler for image files and add a corresponding field in the stract for hero images.
+struct post
+{
+    std::string title;
+    std::string date;
+    std::string description;
+    std::string content;
+};
+
+/* Query class with individual
+ * resolvers for different query fields. */
+class Query
+{
+    public:
+        // Return an array of posts within a range specified in amount.
+        struct post *allPosts(int amount);
+};
+
 #endif /* QUERIES_H */
