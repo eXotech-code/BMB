76d3512086387aa7baeb1bf7ea4b71f94f859e75
Jakub Piskiewicz <jakub@piskiewicz.org>
2021-06-06 15:53:17 +0200

    Separating parameters and removing html spaces.

--- src/modules/frontend/queries.cpp
+++ src/modules/frontend/queries.cpp
@@ -1,6 +1,6 @@
 #include "queries.h"
 
-std::string extract_query(std::string b)
+std::string extract_query(const std::string &b)
 {
 	std::string::size_type pos = b.find("query="); // Position at which query begins
 	if (pos != std::string::npos)
@@ -8,7 +8,7 @@ std::string extract_query(std::string b)
 	return "";
 }
 
-int get_id(const std::string &query)
+int get_id(const std::string &query, std::string::size_type &end_id_pos)
 {
 	// Fragment of text that always precedes id number
 	std::string pre_id = "(id:%20";
@@ -17,9 +17,47 @@ int get_id(const std::string &query)
 
 	// Beginning of id number
 	std::string::size_type b = query.find(pre_id) + pre_id.size();
-	std::cout << query.substr(b, id_len) << " " << std::stoi(query.substr(b, id_len)) << "\n";
+	if (b == std::string::npos)
+	    return -1;
 
-	return 0;
+	end_id_pos = b + id_len;
+
+	return std::stoi(query.substr(b, id_len));
+}
+void removeOccurences(const std::string &seq, std::string &s)
+{
+    size_t start_pos = 0;
+    while ((start_pos = s.find(seq, start_pos)) != std::string::npos)
+    {
+        s.replace(start_pos, seq.length(), " ");
+        start_pos += seq.length();
+    }
+}
+
+std::vector<std::string> separate_args(std::string params)
+{
+    // Remove spaces
+    std::string url_space = "%20";
+    removeOccurences(url_space, params);
+
+    std::vector<std::string> args;
+
+    // Separate arguments into different places in the vector.
+    std::stringstream ss(params);
+    std::string word;
+    while (ss >> word)
+    {
+        args.push_back(word);
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+
+    return args;
+}
+
+struct post *compile_request(std::vector<std::string> args)
+{
+
 }
 
 int resolve_query(char *buff)
@@ -29,11 +67,27 @@ int resolve_query(char *buff)
 	std::string q = extract_query(b);
 
 	// Query is not present or has wrong formatting
-	if (q == "")
+	if (q.empty())
 		return -1;
 
 	std::cout << q << "\n";
 
-	get_id(q);
+	std::string::size_type end_id_pos = 0; // Location of the end of id field.
+	int id = get_id(q, end_id_pos);
+
+	// Id field not present
+	if (id == -1)
+	    return -1;
+
+	// Query parameters
+    std::string params = q.substr(end_id_pos+2);
+
+    // DEBUG: Just checking if separating arguments works as expected
+    std::vector<std::string> p_separated = separate_args(params);
+    for (std::vector<std::string>::const_iterator i = p_separated.begin(); i < p_separated.end(); i++)
+    {
+        std::cout << "from vec: " << *i << "\n";
+    }
+
 	return 0;
 }
--- src/modules/frontend/queries.h
+++ src/modules/frontend/queries.h
@@ -2,20 +2,41 @@
 #ifndef QUERIES_H
 
 #include <string>
+#include <sstream>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 /* This file contains all of the logic
  * needed to parse HTTP requests and queries
  * from the frontend. */
 
 // Returns query extracted from raw buffer
-std::string extract_query(std::string b);
+std::string extract_query(const std::string &b);
 
 // Retrieve id from argument field in query
-int get_id(const std::string &query);
+int get_id(const std::string &query, std::string::size_type &end_id_pos);
+
+/* Struct to be sent to the main module
+ * and filed with data. */
+struct post {
+    unsigned int id;
+    std::string title;
+    std::string date;
+    std::string description;
+    std::string hero;
+    std::string content;
+};
+
+// Replaces all occurences of a string with a space.
+void removeOccurences(const std::string &seq, std::string &s);
+
+std::vector<std::string> separate_args(std::string params);
+// This will only make requests for posts for now
+struct post *compile_request(std::vector<std::string> args);
 
 /* Parses the query and makes calls to resolver functions.
  * It returns a pointer to struct with results */
-int resolve_query(char *query); // For now it just return 0 on succes and -1 on error
+int resolve_query(char *query); // For now it just return 0 on success and -1 on error
 
 #endif /* QUERIES_H */
